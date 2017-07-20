b5342398dc8d99270cfb4acb7b3c5b32271e777c
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-07-20 11:39:40 +0100

    adding link tracking for share buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import shares from './share'
 
-let shareFn = shares('Where are we up to in these Brexit talks?', 'https://gu.com/p/6mgzm', '');
+let shareFn = shares('Where are we up to in these Brexit talks?', 'https://gu.com/p/6mgzm?CMP=share_btn_tw', '');
 
 [].slice.apply(document.querySelectorAll('.interactive-share')).forEach(shareEl => {
     var network = shareEl.getAttribute('data-network');
