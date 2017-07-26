b36b782d9f5dcefd1deac5f929318bcffe54bdfa
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-07-26 15:15:40 +0100

    more fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import req from 'browser-request'
 import config from '../../config.json'
 
-function maketopicarray (quotes,topics) {
+function maketopicarray(quotes, topics) {
 
-    topics.forEach(function (t){
-        t.rows = quotes.filter(function(r){
+    topics.forEach(function (t) {
+        t.rows = quotes.filter(function (r) {
             return r.topic == t.name;
         })
     })
@@ -14,24 +14,22 @@ function maketopicarray (quotes,topics) {
 function updateRecentMessages(lastvisit) {
     var today = new Date();
     var lastvisitdate = new Date(lastvisit);
-    console.log(lastvisitdate);
 
     // update message counters
     var counters = [].slice.apply(document.querySelectorAll('.gv-counter'));
-    console.log(counters);
-    req(config.docDataJson, function(err,response,body){
+    req(config.docDataJson, function (err, response, body) {
         body = JSON.parse(body);
-        body.sheets.quotes.map(function(q){
+        body.sheets.quotes.map(function (q) {
             var quotedate = new Date(q.datetime);
-            if (quotedate.getFullYear() >= lastvisitdate.getFullYear() && quotedate.getMonth() >= lastvisitdate.getMonth() && quotedate.getDate() > lastvisitdate.getDate() ) {
+            if (quotedate.getFullYear() >= lastvisitdate.getFullYear() && quotedate.getMonth() >= lastvisitdate.getMonth() && quotedate.getDate() >= lastvisitdate.getDate()) {
                 q.new = true;
             }
         })
 
-        var topics = maketopicarray(body.sheets.quotes,body.sheets.topics);
-        topics.forEach(function(t){
-            t.new = t.rows.filter(function(r){return r.new == true}).length;
-            var thistopiccounter = counters.find(function(c){
+        var topics = maketopicarray(body.sheets.quotes, body.sheets.topics);
+        topics.forEach(function (t) {
+            t.new = t.rows.filter(function (r) { return r.new == true }).length;
+            var thistopiccounter = counters.find(function (c) {
                 return c.classList.contains(`gv-topic-${t.name}`);
             })
             console.log(thistopiccounter);
@@ -49,22 +47,23 @@ function updateRecentMessages(lastvisit) {
 
     // update lastvisit (but only if the number of days is greater than one)
     if (today.toDateString() == lastvisitdate.toDateString()) {
-
+        console.log('last visit today; doing nothing')
     } else {
-    localStorage.setItem('gv-last-visit',today)
-   
-}
+        localStorage.setItem('gv-last-visit', today)
+
+    }
 }
 
 
-if (typeof(Storage) !== "undefined") {
+if (typeof (Storage) !== "undefined") {
     var lastvisit = localStorage.getItem('gv-last-visit');
     if (lastvisit) {
-    //    updateRecentMessages('Thu 6 Jul 2017');
+        //    updateRecentMessages('Thu 6 Jul 2017');
         updateRecentMessages(lastvisit);
-    } else   {
+    } else {
         console.log('setting last visit')
-        localStorage.setItem('gv-last-visit', new Date())};
+        localStorage.setItem('gv-last-visit', new Date())
+    };
 } else {
     console.log('no web storage');
     // Sorry! No Web Storage support..
