0248d6a5b2c268d425bc9b2c812bd4cf6ce641f2
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-08-08 08:57:23 +0100

    now with pagedepth tracking

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,26 @@ import shares from './share'
 let shareFn = shares('Where are we up to in these Brexit talks?', 'https://gu.com/p/6mgzm?CMP=share_btn_tw', '');
 
 
+var isScrolling;
+window.addEventListener('scroll', function ( event ) {
+    window.clearTimeout( isScrolling );
+    isScrolling = setTimeout(function() {
+        var h2s = [].slice.apply(document.querySelectorAll('h2'))
+        h2s.forEach(function(h){
+            var position = h.getBoundingClientRect();
+            var sectionnumber = h2s.indexOf(h) + 1;
+            if (position.top > 0 && position.top < window.innerHeight && window.ga){
+                window.ga('create', 'UA-78705427-1', 'auto');
+                window.ga('set', 'dimension3', 'theguardian.com' );
+                window.ga("send", "event", "interactives", "sectiondepth", `section ${sectionnumber} of ${h2s.length}`);
+            }
+        })
+
+    }, 66);
+
+}, false);
+
+
 function expand(button,topic) {
                if (window.ga) {
         window.ga('create', 'UA-78705427-1', 'auto');
